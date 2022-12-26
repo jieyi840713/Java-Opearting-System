3efe74c95e6eec140ace732584fbf64d3010c85e
Jay <jieyi840713@gmail.com>
2022-12-26 12:34:46 +0800

    Threads Collaboration

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import java.sql.Connection;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -8,51 +10,70 @@ public class Main {
     private static Account account = new Account();
 
     private static class Account {
-        private int balance = 0 ;
+        // fields
+        private int balance = 0;
         private static Lock lock = new ReentrantLock();
+        private static Condition newDeposit = lock.newCondition();
 
-        public int getBalance(){
+        public int getBalance() {
             return this.balance;
         }
 
-        //  public synchronized  deposit()
-        //  synchronized java solve race condition
-        public void deposit(){
+        public void deposit(int amount) {
             lock.lock();
-            int newBalance = this.balance+1;
-            try{
-                Thread.sleep(3);
-                this.balance = newBalance;
-            }catch (InterruptedException e){
-                e.printStackTrace();
+            balance += amount;
+            System.out.println("Deposit " + amount + "\t\t\t\t\t" + getBalance());
+            newDeposit.signalAll();
+            lock.unlock();
+        }
+
+        public void withdraw(int amount) {
+            lock.lock();
+
+            try {
+                while (balance < amount) {
+                    System.out.println("\t\t\t\t\t\t\tWait for a deposit");
+                    newDeposit.await();
+                }
+                balance -= amount;
+                System.out.println("\t\t\t\t\t\t\tWithdraw " + amount + "\t\t" + getBalance());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             } finally {
                 lock.unlock();
             }
         }
     }
 
-    private static class AddMoneyTask implements Runnable{
+    private static class AddMoneyTask implements Runnable {
 
         @Override
         public void run() {
-//            synchronized (account){
-//                account.deposit();
-//            }
-            account.deposit();
+            try {
+                while (true) {
+                    account.deposit((int) (Math.random() * 20) + 5);
+                    Thread.sleep(500);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
+
+    public static class WithdrawTask implements Runnable {
+
+        @Override
+        public void run() {
+            while (true) {
+                account.withdraw((int) (Math.random() * 20) + 50);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        ExecutorService executorService = Executors.newCachedThreadPool();
-
-        // create 50 task;
-        for(int i = 0 ; i< 50 ; i++){
-            executorService.execute(new AddMoneyTask());
-        }
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        executorService.execute(new AddMoneyTask());
+        executorService.execute(new WithdrawTask());
         executorService.shutdown();
-        while (!executorService.isTerminated()){
-
-        }
-
-        System.out.println("The balance is "+ account.getBalance());
     }
 }
